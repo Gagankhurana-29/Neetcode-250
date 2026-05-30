47a166da9247bab659ea05ee69e71ffee4d53ccf
Gagan Khurana <62374992+Gagankhurana-29@users.noreply.github.com>
2026-05-30 13:05:25 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.sort(coins);
+        int n = coins.length;
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                if(coins[j]>i){
+                    break;
+                }
+                if(dp[i-coins[j]] != Integer.MAX_VALUE){
+                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
+    }
+}
