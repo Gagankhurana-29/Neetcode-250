985329e6e45970c77f9b90f1f32e3c2e480a2a66
Gagan Khurana <62374992+Gagankhurana-29@users.noreply.github.com>
2026-08-04 20:16:57 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,37 @@
+class MinStack {
+
+    Stack<Integer> st;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        st = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if(minStack.isEmpty() || minStack.peek()>=val)
+        {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(st.peek().equals(minStack.peek()))
+        {
+            st.pop();
+            minStack.pop();
+        }
+        else{
+            st.pop();
+        }
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
